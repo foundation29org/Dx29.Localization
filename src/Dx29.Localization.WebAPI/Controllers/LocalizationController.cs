13ee263fc2ab3fe2b016510f4287877258833b27
Alvaromah <alvaroma@live.com>
2021-12-14 21:02:45 +0100

    Project startup

---
+++ src/Dx29.Localization.WebAPI/Controllers/LocalizationController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Dx29.Services;
+
+namespace Dx29.Localization.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public class LocalizationController : ControllerBase
+    {
+        public LocalizationController(LocalizationService localizationService)
+        {
+            LocalizationService = localizationService;
+        }
+
+        public LocalizationService LocalizationService { get; }
+
+        [HttpGet("literals")]
+        public async Task<IActionResult> GetLiteralsAsync(string lang = "en")
+        {
+            try
+            {
+                var items = await LocalizationService.GetLiteralsAsync(lang);
+                return Ok(items);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("literal")]
+        public async Task<IActionResult> GetLiteralAsync(string key, string lang = "en")
+        {
+            try
+            {
+                var item = await LocalizationService.GetLiteralAsync(lang, key);
+                return Ok(item);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("literals")]
+        public async Task<IActionResult> SetLiteralsAsync([FromBody] IDictionary<string, string> literals, string lang = "en")
+        {
+            try
+            {
+                await LocalizationService.SetLiteralsAsync(lang, literals);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("literal")]
+        public async Task<IActionResult> SetLiteralAsync([FromBody] KeyValuePair<string, string> literal, string lang = "en")
+        {
+            try
+            {
+                await LocalizationService.SetLiteralAsync(lang, literal);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("register")]
+        public async Task<IActionResult> RegisterLiteralAsync([FromBody] KeyValuePair<string, string> literal, string lang = "en")
+        {
+            try
+            {
+                var value = await LocalizationService.RegisterLiteralAsync(lang, literal.Key);
+                return Ok(value);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("literal")]
+        public async Task<IActionResult> DeleteLiteralAsync(string key, string lang = "en")
+        {
+            try
+            {
+                await LocalizationService.DeleteLiteralAsync(lang, key);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/Dx29.Localization/Services/LocalizationService/LocalizationService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Azure.Storage.Blobs.Models;
+
+namespace Dx29.Services
+{
+    public class LocalizationService
+    {
+        const string CONTAINER = "literals";
+
+        public LocalizationService(TranslationService translationService, BlobStorage blobStorage)
+        {
+            TranslationService = translationService;
+            BlobStorage = blobStorage;
+        }
+
+        public TranslationService TranslationService { get; }
+        public BlobStorage BlobStorage { get; }
+
+        public async Task<IDictionary<string, string>> GetLiteralsAsync(string language)
+        {
+            language = NormalizeLanguage(language);
+            string path = GetLiteralsPath(language);
+            var dic = await BlobStorage.DownloadObjectAsync<IDictionary<string, string>>(CONTAINER, path);
+            dic ??= new Dictionary<string, string>();
+            return dic;
+        }
+
+        public async Task<string> GetLiteralAsync(string language, string key)
+        {
+            var dic = await GetLiteralsAsync(language);
+            return dic.TryGetValue(key);
+        }
+
+        public async Task SetLiteralsAsync(string language, IDictionary<string, string> literals, BlobRequestConditions conditions = null)
+        {
+            language = NormalizeLanguage(language);
+            string path = GetLiteralsPath(language);
+            await BlobStorage.UploadObjectAsync(CONTAINER, path, literals, conditions);
+        }
+
+        public async Task SetLiteralAsync(string language, KeyValuePair<string, string> literal)
+        {
+            await SetLiteralAsync(language, literal.Key, literal.Value);
+        }
+        public async Task SetLiteralAsync(string language, string key, string value)
+        {
+            language = NormalizeLanguage(language);
+            string path = GetLiteralsPath(language);
+
+            var leaseClient = BlobStorage.GetLeaseClient(CONTAINER, path);
+            var lease = await BlobStorage.AcquireLeaseAsync(leaseClient);
+            var conditions = new BlobRequestConditions { LeaseId = lease.LeaseId };
+            var dic = await GetLiteralsAsync(language);
+            dic[key] = value;
+            await SetLiteralsAsync(language, dic, conditions);
+            await leaseClient.ReleaseAsync();
+        }
+
+        public async Task<string> RegisterLiteralAsync(string language, string key)
+        {
+            var dic = await GetLiteralsAsync(language);
+            if (!dic.ContainsKey(key))
+            {
+                string value = await TranslationService.TranslateAsync(key, language);
+                await SetLiteralAsync(language, key, value);
+                return value;
+            }
+            return dic[key];
+        }
+
+        public async Task DeleteLiteralAsync(string language, string key)
+        {
+            var dic = await GetLiteralsAsync(language);
+            if (dic.ContainsKey(key))
+            {
+                dic.Remove(key);
+                await SetLiteralsAsync(language, dic);
+            }
+        }
+
+        private string GetLiteralsPath(string language) => $"{language}.json";
+
+        private string NormalizeLanguage(string language)
+        {
+            switch (language.ToLowerInvariant())
+            {
+                case "es":
+                case "es-es":
+                    return "es-es";
+                case "fr":
+                case "fr-fr":
+                    return "fr-fr";
+                default:
+                    return "en-us";
+            }
+        }
+    }
+}
